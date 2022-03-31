f5af296e1b4cf57118d52ef539aa3a066ea7a509
okaka <aki.na@live.jp>
2022-03-31 10:23:00 +0900

    ボールゲーム修正

--- NagatsuMain.java
+++ NagatsuMain.java
@@ -11,11 +11,20 @@ public class NagatsuMain{
 			balls[index] = balls[i];
 			balls[i] = tmp;
 		}
+		int sumA = 0;
+		int sumB = 0;
 		for(int i=0;i<5;i++){
-		System.out.println((i+1)+"回戦");
-		System.out.printf("A:%d,B:%d...",balls[2*i],balls[2*i+1]);
-		System.out.print(balls[2*i]>balls[2*i+1]?"A":"B");
-		System.out.println("の勝ち");
+			System.out.println((i+1)+"回戦");
+			System.out.printf("A:%d,B:%d...",balls[2*i],balls[2*i+1]);
+			System.out.print(balls[2*i]>balls[2*i+1]?"A":"B");
+			System.out.println("の勝ち");
+			if(balls[2*i]>balls[2*i+1]){
+				sumA++;
+			}else{sumB++;
+			}
 		}
+			System.out.printf("%d対%dで",sumA,sumB);
+			System.out.print(sumA>sumB?"A":"B");
+			System.out.println("の勝ち");
 	}
 }
